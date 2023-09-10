bf1b4056327157d2cc47015e516241a629e41bee
Joel <58995538+jtpotato@users.noreply.github.com>
2023-09-10 21:17:14 +1000

    add easter egg

--- src/boba-components/GlitchyTextOnScroll.tsx
+++ src/boba-components/GlitchyTextOnScroll.tsx
@@ -13,10 +13,32 @@ function GlitchyTextOnScroll() {
 
     const randomChar = () =>
       chars[Math.floor(Math.random() * (chars.length - 1))];
-    const randomString = (length: number) =>
-      Array.from(Array(length)).map(randomChar).join("");
+    const randomString = (length: number) => {
+      let theRandomString = Array.from(Array(length)).map(randomChar).join("");
+      // put some easter egg somewhere in the middle of that string
+      theRandomString =
+        theRandomString.slice(0, 2000 * Math.random() + 1000) + "boba!".repeat(80) + "honestlyiwouldnotexpectanyonetoreadthis" + theRandomString.slice(4000);
+      return theRandomString;
+    };
     if (glitchTextRef.current == null) return;
+
     glitchTextRef.current.innerText = randomString(8000);
+
+    let hasScrolled = false;
+
+    window.onscroll = () => (hasScrolled = true);
+
+    const scrollChecker = setInterval(() => {
+      if (!hasScrolled) return;
+      if (!glitchTextRef.current) return;
+      glitchTextRef.current.innerText = randomString(8000);
+      hasScrolled = false;
+    }, 10);
+
+    return () => {
+      clearInterval(scrollChecker);
+      window.onscroll = null;
+    };
   }, []);
 
   return (
