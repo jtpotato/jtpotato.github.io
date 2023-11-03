96c50d82964e0605e8557f0455b2bc94de5d1078
Joel <58995538+jtpotato@users.noreply.github.com>
2023-11-04 10:45:39 +1100

    add first test page

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,13 @@
+function Posts() {
+    return (
+        <>
+            <div className="flex justify-center">
+                <main className="p-4 max-w-4xl w-full">
+                    <p>Recent posts:</p>
+                </main>
+            </div>
+        </>
+    );
+}
+
+export default Posts;
