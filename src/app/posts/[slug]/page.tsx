e214dfda253903e7e47ff7e841f22d1c932bb87c
Joel <58995538+jtpotato@users.noreply.github.com>
2023-11-04 13:08:17 +1100

    update stuff

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -8,8 +8,10 @@ function PostPage({ params }: { params: { slug: string } }) {
     <>
       <div className="flex justify-center">
         <div className="max-w-2xl w-full p-4">
-          <h1 className="text-6xl font-bold">{post?.title}</h1>
-          <p className="text-gray-500">Published {post?.published}</p>
+          <h1 className="text-5xl font-bold mb-4">{post?.title}</h1>
+          <p className="text-gray-500 m-0">Published {post?.published}</p>
+          <p className="text-gray-500 m-0">Edited {post?.edited}</p>
+          <div className="h-[1px] bg-gray-500 my-4" />
           <RemoteMdxPage contents={post!.content} />
         </div>
       </div>
