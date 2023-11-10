1d67ad39679dd4bd99be725423dc59d9fd59b9f8
Joel <58995538+jtpotato@users.noreply.github.com>
2023-11-10 20:49:23 +1100

    update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FancyBackground from "@/lib/homepage/FancyBackground";
 import Link from "next/link";
 
 export default function Home() {
@@ -5,9 +6,12 @@ export default function Home() {
     <main>
       <div className="flex justify-center items-center h-screen">
         <div className="flex flex-col items-center">
-          <h1 className="text-4xl">Hi, I&apos;m Joel</h1>
+          <h1 className="text-6xl font-bold">Hi, I&apos;m Joel</h1>
           <div className="h-32"></div>
+          <div className="border border-black/50 p-4 px-8">
           <Link href="/posts">Posts</Link>
+          </div>
+
         </div>
       </div>
     </main>
