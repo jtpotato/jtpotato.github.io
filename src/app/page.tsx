4f119ee079969652a165649187e83d3503791f44
Joel <58995538+jtpotato@users.noreply.github.com>
2023-11-15 14:28:58 +1100

    add small projects page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
           <h1 className="text-6xl font-bold">Hi, I&apos;m Joel</h1>
           <div className="h-32"></div>
           <div className="flex">
-            <div className="border border-lines border-l border-t border-b p-4 px-8">
+            <div className="w-32 border-lines border-l border-t border-b p-4 px-8 flex justify-center">
               <Link href="/projects">Projects</Link>
             </div>
-            <div className="border border-lines p-4 px-8">
+            <div className="w-32 flex justify-center border border-lines p-4 px-8">
               <Link href="/posts">Posts</Link>
             </div>
           </div>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,24 @@
+import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
+
+import "./empty.css"
+
 function Projects() {
-    return (<>
-        <p>Projects</p>
-    </>);
+  return (
+    <>
+      <div className="w-full flex justify-center items-center p-4">
+        <div className="max-w-2xl w-full">
+          <Link href="/projects/firetrace">
+            <div className="flex items-center space-x-2">
+              <p className="text-2xl font-bold">Firetrace</p>
+              <ArrowUpRight />
+            </div>
+          </Link>
+          <p className="text-neutral-400">Bushfire prediction AI.</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Projects;
