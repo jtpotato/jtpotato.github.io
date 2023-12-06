117a9e8bae7cab2028781c11f1a760a56f9e0c13
Joel <58995538+jtpotato@users.noreply.github.com>
2023-12-06 17:51:54 +1100

    properly add fonts

---
+++ src/app/HomePage.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+export default function HomePage() {
+    return (
+      <>
+        <div>
+          <p className='font-display font-bold text-8xl'>I'm Joel</p>
+        </div>
+      </>
+    )
+  }
+  
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      fontFamily: {
+        body: ['var(--font-satoshi)', 'sans-serif'],
+        display: ['var(--font-general-sans)', 'sans-serif']
+      }
     },
   },
   plugins: [],
