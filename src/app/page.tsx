3aeb8eb6a42828b1897fd6575f3d15f437d84746
Joel <58995538+jtpotato@users.noreply.github.com>
2023-12-07 20:23:13 +1100

    rearrange metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import HomePage from "./HomePage";
+
+export const metadata: Metadata = {
+  title: 'jtpotato',
+  description: 'Hi - I\'m Joel, a student, developer and content creator based in Melbourne, Australia.',
+}
 
 export default function Home() {
   return (
