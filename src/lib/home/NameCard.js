6ce9cb493fdfcdf1306d2ced8225c9ea5eb27d71
Joel <58995538+jtpotato@users.noreply.github.com>
2023-12-22 17:35:52 +1100

    add background animation

--- src/lib/home/NameCard.js
+++ src/lib/home/NameCard.js
@@ -4,6 +4,11 @@ function NameCard() {
   return (
     <>
       <div className={styles.parent}>
+        <div className={styles.background}>
+          <span className={styles.aurora1}></span>
+          <span className={styles.aurora2}></span>
+          <span className={styles.aurora3}></span>
+        </div>
         <h1 className={styles.name}>I'm Joel</h1>
       </div>
     </>
