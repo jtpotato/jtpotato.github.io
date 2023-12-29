5c829525bd175d5c85ea373f59c48ebda49419eb
Joel <58995538+jtpotato@users.noreply.github.com>
2023-12-29 21:42:52 +1100

    add cloudflare analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,24 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'jtpotato',
-  description: 'My personal site. Welcome!',
-}
+  title: "jtpotato",
+  description: "My personal site. Welcome!",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "328e28981c8044cca1b839a09b0e30d1"}'
+        ></script>
+      </body>
     </html>
-  )
+  );
 }
