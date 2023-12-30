37e1fd20892167433ba98f2886e14762db7ba05d
Joel <58995538+jtpotato@users.noreply.github.com>
2023-12-30 12:14:00 +1100

    try rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  async rewrites() {
+    return [
+      {
+        source: "/posts",
+        destination: "https://jtpotato-posts.netlify.app"
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
