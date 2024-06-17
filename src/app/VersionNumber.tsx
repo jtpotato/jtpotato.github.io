3590622ca0d9c8b0fe74bee9fe1549135cdcc543
Joel <58995538+jtpotato@users.noreply.github.com>
2024-06-17 17:47:45 +1000

    update all the files.

--- src/app/VersionNumber.tsx
+++
@@ -1,9 +0,0 @@
-function VersionNumber() {
-  return (<>
-    <div className="fixed bottom-8 left-8 text-secondary text-sm bg-gradient-to-br from-white/10 to-neutral-400/10 px-2 py-1 rounded-full border-neutral-100 border backdrop-blur-md">
-      <p>v2024.3.28</p>
-    </div>
-  </>);
-}
-
-export default VersionNumber;
