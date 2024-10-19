9e3629eead204cd9b3d4028c583c435f31a6524e
Joel <58995538+jtpotato@users.noreply.github.com>
2024-10-19 13:09:52 +1100

    update again

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import "./style.css";
+</script>
+
+<template>
+  <div class="content-main">
+    <h1>Joel Tan / 陈奕諴</h1>
+    <p>
+      I'm a student from Melbourne, Australia. Earth science and programming
+      enthusiast. Advocate for STEM education at all ages.
+    </p>
+    <br />
+    <h2>How did I get here?</h2>
+    <p>
+      I started with robotics in 2016. Competing internationally opened my mind
+      to the various opportunities that programming offers.
+    </p>
+    <p>
+      During the pandemic, I spent time building projects that solved problems
+      that I had.
+    </p>
+    <footer>
+      <p class="footer-text" style="width: fit-content; margin: auto">
+        Made with ❤️ and 🧋 by Joel Tan
+      </p>
+    </footer>
+  </div>
+</template>
